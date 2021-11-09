c45694fedbf7c73737933d5c6b27e1f94a1f8ea1
jonathanb88 <jonhybw@gmail.com>
2021-11-09 14:46:58 -0300

    Routes

---
+++ e-commerce-front/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
