d1e51c084b4a4686bfeab92d76f475d11b466183
jonathanb88 <jonhybw@gmail.com>
2021-11-10 15:45:45 -0300

    eliminacion de footer.css

--- e-commerce-front/src/pages/Cart.jsx
+++ e-commerce-front/src/pages/Cart.jsx
@@ -1,11 +1,80 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/NavBar";
+import { Col, Container, Row, Button, Table } from "react-bootstrap";
 
 function Cart() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>CART</h1>
-    </div>
+      <div className="cart">
+        <Container className="my-4 py-4">
+          <Row>
+            {/* Table of products in cart */}
+            <Col>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>PRODUCT</th>
+                    <th>PRICE</th>
+                    <th>QUANTITY</th>
+                    <th>SUBTOTAL</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+
+            {/* Total numbers */}
+            <Col className="border border-2 py-4">
+              <p>
+                <strong>TOTAL FROM CART</strong>
+              </p>
+              <hr />
+              <div>
+                <div className="d-flex justify-content-between">
+                  <p>Subtotal</p>
+                  <p>
+                    <strong>$2.000</strong>
+                  </p>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <p>Total</p>
+                  <strong>$2.000</strong>
+                </div>
+              </div>
+              <div className="btn-block">
+                <Button
+                  variant="dark"
+                  type="button"
+                  className="my-3 rounded-pill px-4 btn-block"
+                >
+                  Proceed to checkout
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Footer />
+    </>
   );
 }
 
