adc30b03eb058a88a460e41b193a6370a6e66278
ignacionin <ignaninm@gmail.com>
2021-11-10 11:35:13 -0300

    carusel

---
+++ e-commerce-front/src/components/Carusel.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./Carusel.css";
+import Button from "react-bootstrap/Button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Controller,
+  Thumbs,
+} from "swiper";
+
+import "swiper/swiper-bundle.css";
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+SwiperCore.use([Autoplay, Navigation, Pagination, Controller, Thumbs]);
+
+const data = [
+  {
+    id: 1,
+    name: "Sabanera",
+    price: "550",
+    image:
+      "https://cultocafe.uy/wp-content/uploads/2020/03/DSC09435-600x750.jpg",
+  },
+  {
+    id: 2,
+    name: "Mamba Negra",
+    price: "750",
+    image:
+      "https://cultocafe.uy/wp-content/uploads/2020/03/DSC09429-600x750.jpg",
+  },
+  {
+    id: 3,
+    name: "Naja",
+    price: "650",
+    image:
+      "https://cultocafe.uy/wp-content/uploads/2020/03/DSC09428-600x750.jpg",
+  },
+  {
+    id: 4,
+    name: "Cascabel",
+    price: "500",
+    image:
+      "https://cultocafe.uy/wp-content/uploads/2020/03/DSC09427-600x750.jpg",
+  },
+];
+
+function Carusel() {
+  return (
+    <div>
+      <h3 className="text-center title mb-4">COFFEE</h3>
+      <div className="carusel">
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={3}
+          navigation
+          // pagination={{ clickable: true }}
+          //scrollbar={{ draggable: true }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {data.map((product) => (
+            <SwiperSlide key={product.id} className="slide">
+              <div className="slide-content">
+                <div className="product-image">
+                  <img className="product-photo" src={product.image} alt="" />
+                </div>
+                <div className="carusel-details">
+                  <h3 className="text-center">{product.name}</h3>
+                  <p className="text-center" id="precioCarusel">
+                    {product.price}
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="d-flex justify-content-center ">
+        <Button
+          className="btn-lg mt-5 mb-5 button-carusel rounded-pill"
+          variant="dark"
+        >
+          SEE ALL
+        </Button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default Carusel;
--- e-commerce-front/src/pages/Home.jsx
+++ e-commerce-front/src/pages/Home.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/NavBar";
 import Header from "../components/Header";
+import Carusel from "../components/Carusel";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Header />
-
+   <Carusel />
       <Footer />
     </>
+   
+  
   );
 }
 
