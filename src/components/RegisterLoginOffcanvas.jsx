5fa97c1ac3d985ef20af7a0e3022999618dcce0e
jonathanb88 <jonhybw@gmail.com>
2021-11-12 15:03:09 -0300

    login check

--- src/components/RegisterLoginOffcanvas.jsx
+++ src/components/RegisterLoginOffcanvas.jsx
@@ -61,9 +61,25 @@ function OffCanvasExample({ name, ...props }) {
 
   return (
     <>
-      <Nav.Link onClick={handleShow}>
-        <i className="far fa-user pointer text-muted"></i>
-      </Nav.Link>
+      {user.token ? (
+        <Nav.Link onClick={handleShow}>
+          <i className="far fa-user pointer text-muted"></i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="15"
+            height="15"
+            viewBox="0 0 36 36"
+          >
+            <path d="M0 12.116l2.053-1.897c2.401 1.162 3.924 2.045 6.622 3.969 5.073-5.757 8.426-8.678 14.657-12.555l.668 1.536c-5.139 4.484-8.902 9.479-14.321 19.198-3.343-3.936-5.574-6.446-9.679-10.251z" />
+          </svg>{" "}
+        </Nav.Link>
+      ) : (
+        <Nav.Link onClick={handleShow}>
+          {" "}
+          <i className="far fa-user pointer text-muted"></i>{" "}
+        </Nav.Link>
+      )}
+
       <Offcanvas
         show={show}
         onHide={() => {
