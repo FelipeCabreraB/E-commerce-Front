39b6f234f6606f9acb9e1a28e237da5932f41b25
FelipeCabreraB <felipemartincabrera@gmail.com>
2021-11-25 22:32:45 -0300

    loader header

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,14 +1,29 @@
-//import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 import VideoCulto from "../../videos/Culto-Web-v1-optimizado.mp4";
 import "./Header.css";
 
 function Header() {
-  //const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div>
-      <video id="header-video" loop autoPlay muted>
+      {isLoading && (
+        <div
+          className="w-100 d-flex justify-content-center"
+          style={{ paddingTop: "15vh" }}
+        >
+          <Spinner animation="border" variant="dark" />
+        </div>
+      )}
+      <video
+        id="header-video"
+        loop
+        autoPlay
+        muted
+        onLoadedData={() => setIsLoading(false)}
+      >
         <source src={VideoCulto} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
